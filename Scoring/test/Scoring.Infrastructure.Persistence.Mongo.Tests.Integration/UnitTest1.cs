6e462477398bdbe70f7370b233979465d2ca2cf6
ahmad.aghazadeh <Ahmad.aghazadeh.a@gmail.com>
2023-09-16 23:00:53 +0330

    Fix mongodb mapping

--- Scoring/test/Scoring.Infrastructure.Persistence.Mongo.Tests.Integration/UnitTest1.cs
+++ Scoring/test/Scoring.Infrastructure.Persistence.Mongo.Tests.Integration/UnitTest1.cs
@@ -1,5 +1,6 @@
 
 using Framework.Core.Specifications;
+using Framework.Persistence.Mongo;
 using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
 using Scoring.Domain.Model.Applicants;
@@ -13,28 +14,47 @@ namespace Scoring.Infrastructure.Persistence.Mongo.Tests.Integration
         [Fact]
         public async void Test1()
         {
-            BsonClassMap.RegisterClassMap<Specification<Applicant>>(map =>
-            {
-                map.AutoMap();
-                map.SetIsRootClass(true);
-
-                map.AddKnownType(typeof(WorkingExperience));
-                map.AddKnownType(typeof(AndSpecification<Applicant>));
-                map.AddKnownType(typeof(OrSpecification<Applicant>));
-
-            });
+            MappingRegistration.RegisterAll(typeof(UnitTest1).Assembly);
 
             var client = new MongoClient("mongodb://root:changeit@localhost");
             var database = client.GetDatabase("Scoring");
             var repository = new MongoRuleRepository(database);
-            //var spec = new WorkingExperience(TimeSpan.FromDays(365))
-            //    .And(new WorkingExperience(TimeSpan.FromDays(10)));
-            //var rule = new Rule(Guid.NewGuid(), "Test Rule", spec);
+            var spec = new WorkingExperience(TimeSpan.FromDays(365))
+                .And(new WorkingExperience(TimeSpan.FromDays(10)));
+            var rule = new Rule(Guid.NewGuid(), "Test Rule", spec);
 
-            //await repository.Add(rule);
+            await repository.Add(rule);
 
-            var ru =await  repository.Get(Guid.Parse("742c4757-5c67-8346-9230-ebc8fc247040"));
+            var ru = await repository.Get(rule.Id);
 
-        } 
+        }
+
+        public class RuleMapping : IBsonMapping
+        {
+            public void Register()
+            {
+
+                BsonClassMap.RegisterClassMap<Specification<Applicant>>(map =>
+                {
+                    map.AutoMap();
+
+                    map.AddKnownType(typeof(WorkingExperience));
+                    map.AddKnownType(typeof(AndSpecification<Applicant>));
+                    map.AddKnownType(typeof(OrSpecification<Applicant>));
+                });
+
+                BsonClassMap.RegisterClassMap<CompositeSpecification<Applicant>>(map =>
+                {
+                    map.AutoMap();
+                    map.SetIsRootClass(true);
+                    map.AddKnownType(typeof(AndSpecification<Applicant>));
+                    map.AddKnownType(typeof(OrSpecification<Applicant>));
+                });
+
+               
+
+            }
+
+        }
     }
 }
